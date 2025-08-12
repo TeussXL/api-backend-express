aa975182df73a3695d3bd49d3f1580c258166304
Mateus Rodrigo <mateus.rodrigo@aluno.ifsp.edu.br>
2025-08-12 17:47:09 -0300

    aprendizado dos cruds

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Comando
+# Comando git 
 
 - npm init // cria o package.json para iniciar o projeto node
 - npm i nome_pacote // instala um pacotes especifico
 - npm install ou npm i // instala todos os pacotes do package.jsonn
+ou
+npm run nome_do_script // executa um script do package.json 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,40 @@
-import express from 'express'
+import express from 'express' // Importando o módulo express
 
-const express = require('express')
-const app = express()
-const port = 3000
+const app = express() // Criando uma instância do express
+const port = 3000 // Definindo a porta do servidor
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json()) // Middleware para analisar JSON
+
+// app.put('/user', (req, res) => {
+// //pegar dados do usuário
+// res.json({message: 'Usuario atualizado com sucesso' })
+// })
+
+app.post('/profile', (req, res) => {
+const dados = req.body
+res.json({
+message: 'Perfil criado com sucesso',
+profile: dados
+})
 })
 
+app.get('/profile', (req, res) => {
+res.json({ message: 'Perfil retornado com sucesso' })
+})
+
+app.put('/profile', (req, res) => {
+const dados = req.body
+res.json({
+message: 'Perfil atualizado com sucesso',
+profile: dados
+})
+})
+
+app.delete('/profile', (req, res) => {
+res.json({ message: 'Perfil deletado com sucesso' })
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+console.log(`Example app listening on port http://localhost:${port}`)
 })
