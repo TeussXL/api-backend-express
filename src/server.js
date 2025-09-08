07fe4440932adf325d00cf5cf090c4ba88045025
Mateus Rodrigo <mateus.rodrigo@aluno.ifsp.edu.br>
2025-09-08 17:42:36 -0300

    att

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ import productRouter from './routers/productRouter.js'
 import supplierRouter from './routers/supplierRouter.js'
 import customerRouter from './routers/customerRouter.js'
 import carRouter from './routers/carRouter.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3333
 
+app.use(cors()) // Habilitar o CORS para todas as rotas e todos os domínios 
 app.use(express.json()) // Converter o JSON que chegou na requisição em um objeto js e vai salvar em req.body
 
 app.use('/profile', profileRouter)
