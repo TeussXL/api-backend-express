216bb1e3b7c757f8f3bd7f46424793fa79285968
Mateus Rodrigo <mateus.rodrigo@aluno.ifsp.edu.br>
2025-11-18 16:57:28 -0300

    validação de dados com zod

--- src/controllers/profile/createProfileController.js
+++ src/controllers/profile/createProfileController.js
@@ -1,13 +1,23 @@
-import { create } from '../../models/profileModel.js';
-import bcrypt from 'bcrypt';
+import { create, validateProfile } from '../../models/profileModel.js'
+import bcrypt from 'bcrypt'
+
 export const createProfileController = async (req, res) => {
-  const profile = req.body;
+  const profile = req.body
 
-  profile.pass = await bcrypt.hash(profile.pass, 10);
+  const validation = validateProfile(profile, {id: true})
+  
+  //verifica se a validação falhou
+  if (!validation.success) {
+    return res.status(400).json({
+      message: 'Dados inválidos',
+      errors: validation.errors
+    })
+  }
+  validation.data.pass = await bcrypt.hash(validation.data.pass, 10)
 
-  const dados = await create(profile);
+  const result = await create(validation.data)
   res.json({
-    message: 'Profile criado com sucesso!',
-    profile: dados
+    message: 'Usuário criado com sucesso!',
+    profile: result
   })
 }
--- src/controllers/profile/deleteProfileController.js
+++ src/controllers/profile/deleteProfileController.js
@@ -1,10 +1,20 @@
-import { remove } from '../../models/profileModel.js'
+import { remove, validateProfile } from '../../models/profileModel.js'
 
 export const deleteProfileController = async (req, res) => {
   const id = req.params.id
 
-  const result = await remove(+id)
+  const validation = validateProfile({ id: +id }, { name: true, email: true, pass: true})
+  if (!validation.success) {
+    return res.status(400).json({
+      message: 'Dados inválidos',
+      errors: validation.errors
+    })
+  }
 
+  if (req.userLogged.id !== validation.data.id) {
+    return res.status(403).json({ message: 'Você não tem permissão para editar este perfil.' })
+  }
+  const result = await remove(validation.data.id) 
   res.json({message: `Usuário com ID ${id} deletado com sucesso!`,
     profile: result
   })
